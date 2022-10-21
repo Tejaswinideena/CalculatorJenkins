f2dbb56290dc17d19bc5f4db160f8db1e861b79a
Tejaswinideena <tejaswinideena14@gmail.com>
2022-10-21 15:37:20 +0530

    Add project files.

---
+++ CalculatorJenkins/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorJenkins
+{
+    public class Calculator
+    {
+        public int Add(int i, int j)
+        {
+            return i + j;
+        }
+        public int Sub(int i, int j)
+        {
+            return i - j;
+        }
+        public int Mul(int i, int j)
+        {
+            return i * j;
+        }
+        public int Div(int i, int j)
+        {
+            return i / j;
+        }
+    }
+}
